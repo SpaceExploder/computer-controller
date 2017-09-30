848cd8fbd4b61db916fb4dc6daa6f865a8cef4ad
SpaceExploder <99danielh@gmail.com>
2017-09-30 14:13:25 -0400

    Theme Chooser

---
+++ script.js
@@ -0,0 +1,17 @@
+$(document).ready(() => {
+	function loadStyleSheet(value) {
+		$('#theme').remove();
+
+		var src = 'https://maxcdn.bootstrapcdn.com/bootswatch/3.3.7/'+value+'/bootstrap.min.css';
+		if (document.createStyleSheet){
+			document.createStyleSheet(src);
+		}
+		else {
+			$("head").append($("<link rel='stylesheet' href='"+src+"' type='text/css' id='theme' />"));
+		}
+	};
+
+	$("#theme-chooser").on('change', function() {
+		loadStyleSheet($(this).val());
+	});
+})
